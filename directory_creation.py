44d9407b99385902c912fe57e4771377a580db62
israel <quirolaisraela@gmail.com>
2023-12-05 19:59:56 -0500

    Challenge 7

---
+++ directory_creation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Import libraries
+
+import os
+
+# Declaration of variables
+
+user_path = input("Enter directory path: ")
+
+### Read user input here into a variable
+
+# Declaration of functions
+def generate_directory_structure(directory_path):
+    for (root, dirs, files) in os.walk(directory_path):
+        
+        print(root)
+        
+        print(dirs)
+        
+        print(files)
+
+
+# Main
+### Pass the variable into the function here
+
+generate_directory_structure(user_path)
+
+# End
